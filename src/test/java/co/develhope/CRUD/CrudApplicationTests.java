3c88abb54acf30da5dd8947422024a6edfe19984
ncorbu1 <paote6@yahoo.it>
2023-04-06 04:57:36 +0200

    CRUD

--- src/test/java/co/develhope/CRUD/CrudApplicationTests.java
+++ src/test/java/co/develhope/CRUD/CrudApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.SpringDemo;
+package co.develhope.CRUD;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringDemoApplicationTests {
+class CrudApplicationTests {
 
 	@Test
 	void contextLoads() {
