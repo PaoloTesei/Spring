fafc217e96b408f9d3f3562070238437d1381012
ncorbu1 <paote6@yahoo.it>
2023-04-06 02:40:47 +0200

    Swagger 1

--- src/main/java/com/example/SpringDemo/controllers/DefaultController.java
+++ src/main/java/com/example/SpringDemo/controllers/DefaultController.java
@@ -4,19 +4,23 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/name")
-public class NameController {
-    @Operation(summary = "Get name", description = "This controllers returns the name")
+@RequestMapping("/api")
+public class DefaultController {
+
+    @GetMapping("/")
+    @Operation(summary = "Welcome!", description = "This controller welcomes the user")
     @ResponseStatus(HttpStatus.OK)
     @ApiResponses({
             @ApiResponse(responseCode = "400",description = "BAD DATA"),
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR"),
             @ApiResponse(responseCode = "200",description = "CREATED")})
-    @GetMapping("/name")
-    public String getName(@RequestParam String name){
-        return name;
+    public String welcome(){
+        return "Welcome!";
     }
 }
